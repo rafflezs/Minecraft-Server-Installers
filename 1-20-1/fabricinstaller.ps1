4cf035dc314d98feb679c4d76f18ce1a30636285
Rafael <50029136+rafflezs@users.noreply.github.com>
2024-09-01 14:58:10 -0300

    Update fabricinstaller.ps1

--- 1-20-1/fabricinstaller.ps1
+++ 1-20-1/fabricinstaller.ps1
@@ -1,10 +1,8 @@
-# DEFINIR VARIAVEIS
-## Java
+# Definindo variaveis
 $javaUrl = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=250111_d8aa705069af427f9b83e66b34f5e380"
 $installerPath = "$env:TEMP\JavaSetup8u421.exe"
 $installPath = "C:\Program Files\Java\jre1.8.0_421"
 
-## Fabric
 $fabricInstallerUrl = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar"
 $minecraftVersion = "1.20.1"
 $modsZipUrl = "https://drive.google.com/uc?export=download&id=1y7jDMrf2edKTNHHqqV1DLG9YRZgRpG_J"
@@ -12,8 +10,7 @@ $minecraftDir = "$env:APPDATA\.minecraft"
 $modsDir = "$minecraftDir\mods"
 $fabricInstallerPath = "$minecraftDir\fabric-installer-1.0.1.jar"
 
-# INSTALACAO
-## Checa se a versao do Java
+# Checando Java
 function Get-JavaVersion {
     try {
         $javaVersion = & java -version 2>&1 | Select-String "version" | ForEach-Object { $_.Line -replace '.*version "', '' -replace '".*', '' }
@@ -23,7 +20,6 @@ function Get-JavaVersion {
     }
 }
 
-## Checa se o Java 8 ou mais ta instalado
 $javaVersion = Get-JavaVersion
 
 if ($javaVersion) {
@@ -40,7 +36,7 @@ if ($javaVersion) {
     $installJava = $true
 }
 
-### Instala o Java 8 se necessario
+# Instalando Java
 if ($installJava) {
     Write-Output "Baixando Java 8 - JRE Oracle..."
     Invoke-WebRequest -Uri $javaUrl -OutFile $installerPath
@@ -56,7 +52,7 @@ if ($installJava) {
     Write-Output "======== INSTALACAO DO JAVA COMPLETA ======="
 }
 
-## Instala o Fabric
+# Preparando Diretorios
 Write-Output "Gerando diretorios do Fabric..."
 if (-not (Test-Path $minecraftDir)) { New-Item -Path $minecraftDir -ItemType Directory }
 if (-not (Test-Path $modsDir)) { New-Item -Path $modsDir -ItemType Directory }
@@ -64,18 +60,23 @@ if (-not (Test-Path $modsDir)) { New-Item -Path $modsDir -ItemType Directory }
 Write-Output "Baixando o instalador Jar do Fabric ..."
 Invoke-WebRequest -Uri $fabricInstallerUrl -OutFile $fabricInstallerPath
 
-## Instala os Mods no %appdata%
+# Instalando Fabric Mod Loader
 Write-Output "Instalando o Fabric Mod Loader..."
 & java -jar $fabricInstallerPath --mcversion $minecraftVersion --noprofile --dir $minecraftDir
 
+# Baixando Modpack
 Write-Output "Baixando modpack (.zip)..."
 Invoke-WebRequest -Uri $modsZipUrl -OutFile "$minecraftDir\mods.zip"
 
 Write-Output "Extraindo mods..."
-Expand-Archive -Path "$minecraftDir\mods.zip" -DestinationPath $modsDir -Force
+if (Test-Path "$minecraftDir\mods.zip") {
+    Expand-Archive -Path "$minecraftDir\mods.zip" -DestinationPath $modsDir -Force
+} else {
+    Write-Output "Arquivo mods.zip não encontrado. Verifique o download."
+}
 
 Write-Output "Limpando..."
 Remove-Item -Path $fabricInstallerPath
 Remove-Item -Path "$minecraftDir\mods.zip"
 
-Write-Output "===== Instalacao completa! ======"
+Write-Output "===== Instalacao completa! ====="
